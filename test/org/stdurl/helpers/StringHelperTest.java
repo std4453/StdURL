1f7daa74fe4a74118896980a31b37c33bbd2f4d4
std4453 <std4453@foxmail.com>
2017-02-03 17:44:06 +0800

    Fix a bug in StringHelperTest.

--- test/org/stdurl/helpers/StringHelperTest.java
+++ test/org/stdurl/helpers/StringHelperTest.java
@@ -39,13 +39,20 @@ public class StringHelperTest {
 		List<Integer> cps = new ArrayList<>();
 
 		Random random = new Random();
-		for (int i = 0; i < 1024; ++i)
-			add(sb, cps, random.nextInt(0x10000)); // BMP
+		for (int i = 0; i < 1024; ++i) {
+			int n = random.nextInt(0x10000); // BMP
+			// filter away high / low surrogates (which will result in an error)
+			if (Character.isHighSurrogate((char) n) ||
+					Character.isLowSurrogate((char) n))
+				continue;
+			add(sb, cps, n);
+		}
 
 		// assertion
 		int[] codePoints = new int[cps.size()];
 		for (int i = 0, size = cps.size(); i < size; ++i)
 			codePoints[i] = cps.get(i);
+
 		assertArrayEquals(codePoints, StringHelper.toCodePoints(sb.toString()));
 	}
 
